2706564591fea162c8ecf3045daac644fe35743f
Krystyna Sharhorodska <rinjara@gmail.com>
2022-08-08 22:32:29 +0300

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,38 +1,42 @@
-// HTML містить <iframe> з відео для Vimeo плеєра.
-// Напиши скрипт, який буде зберігати поточний
-// час відтворення відео у локальне сховище і,
-// після перезавантаження сторінки, продовжувати
-// відтворювати відео з цього часу.
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-
-console.log(iframe);
 const player = new Player(iframe);
 
-console.log(player);
+const CURRENT_TIME = 'videoplayer-current-time';
 
 function currentVideoTimeCollect(data) {
   console.log(data);
-  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(data));
 }
 
 player.on('timeupdate', throttle(currentVideoTimeCollect, 1000));
 
-const savedCurrentTime = localStorage.getItem('videoplayer-current-time');
-const { duration, percent, seconds } = JSON.parse(savedCurrentTime);
-console.log(seconds);
+setSavedTime();
 
-player
-  .setCurrentTime(seconds)
-  .then(function (seconds) {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
+function setSavedTime() {
+  const savedCurrentTime = localStorage.getItem(CURRENT_TIME);
 
-      default:
-        break;
+  if (savedCurrentTime) {
+    try {
+      const { seconds } = JSON.parse(savedCurrentTime);
+      console.log(seconds);
+
+      player
+        .setCurrentTime(seconds)
+        .then(function (seconds) {})
+        .catch(function (error) {
+          switch (error.name) {
+            case 'RangeError':
+              break;
+
+            default:
+              break;
+          }
+        });
+    } catch (error) {
+      console.log(error);
     }
-  });
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,116 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formRef = document.querySelector('.feedback-form');
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const remove = key => {
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+loadPage();
+
+formRef.addEventListener('input', throttle(onInputChange, 500));
+
+function onInputChange(e) {
+  const { name, value } = e.target;
+  let savedData = load(STORAGE_KEY);
+  savedData = savedData ? savedData : {};
+  savedData[name] = value;
+  save(STORAGE_KEY, savedData);
+}
+
+function loadPage() {
+  const savedData = load(STORAGE_KEY);
+
+  if (savedData) {
+    Object.entries(savedData).forEach(([name, value]) => {
+      formRef.elements[name].value = value;
+    });
+  }
+}
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return console.log('Please fill in all the fields!');
+  }
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(formData);
+  remove(STORAGE_KEY);
+  e.currentTarget.reset();
+}
+
+////////   Попередній варіант "для себе", майже все вийшло зробити ////////
+
+// const formData = {};
+
+// const emailRef = document.querySelector('.feedback-form input');
+// const messageRef = document.querySelector('.feedback-form textarea');
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   e.currentTarget.reset();
+//   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function onFormInput(e) {
+//   //   const message = e.target.value;
+//   //   localStorage.setItem(STORAGE_KEY, message);
+//   formData[e.target.name] = e.target.value;
+//   //   console.log(formData);
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function shareSavedInput() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedMessage) {
+//     try {
+//       const parsedMessage = JSON.parse(savedMessage);
+//       console.log(parsedMessage);
+//       emailRef.value = parsedMessage.email || '';
+//       messageRef.value = parsedMessage.message || '';
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   } else '';
+// }
+
+// formRef.addEventListener('input', throttle(onFormInput, 500));
+// formRef.addEventListener('submit', onFormSubmit);
+// shareSavedInput();
+
+// create, read, update, delete
